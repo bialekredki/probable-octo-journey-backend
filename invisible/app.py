4d2ed91ed7852081f191b292c93d3c89f06d4fbc
oskar <oskarkorgul@gmail.com>
2023-04-16 00:04:15 +0200

    feat: Add database to app object

--- invisible/app.py
+++ invisible/app.py
@@ -3,6 +3,7 @@ import logging
 from fastapi import FastAPI
 from fastapi.requests import Request
 from motor.motor_asyncio import AsyncIOMotorClient
+from motor.core import Database
 from motor.core import AgnosticClient
 
 from invisible.config import configuration
@@ -11,12 +12,14 @@ from invisible.config import configuration
 class TypedApp(FastAPI):
     logger: logging.Logger
     db_client: AgnosticClient
+    database: Database
     config = configuration
 
     def __init__(self, **kwargs) -> None:
         super().__init__(docs_url=None, redoc_url="/docs", **kwargs)
         self.logger = logging.getLogger(self.title)
         self.db_client = AsyncIOMotorClient(self.config.mongo_dsn)
+        self.database = self.db_client["test"]
 
 
 class TypedRequest(Request):
