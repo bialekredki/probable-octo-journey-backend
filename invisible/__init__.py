3dc7ee80797addd9cc5667056e1e53fd5f07a91d
oskar <oskarkorgul@gmail.com>
2023-04-16 00:03:55 +0200

    feat: Add index creation onstartup

--- invisible/__init__.py
+++ invisible/__init__.py
@@ -1,8 +1,13 @@
 from invisible.app import TypedApp
 from invisible.routes import bootstrap_routers
+from pymongo import ASCENDING
 
 
 def initialize_application():
     app = TypedApp()
     bootstrap_routers(app)
+
+    @app.on_event("startup")
+    async def _on_startup():
+        await app.database["tinyurl"].create_index([("tiny_url", ASCENDING), ("url", ASCENDING)])
     return app
