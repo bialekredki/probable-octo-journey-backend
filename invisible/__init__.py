5848082f1473b361d38fc448c1abe19ab2c5eea4
oskar <oskarkorgul@gmail.com>
2023-04-15 15:23:38 +0200

    chore: Make app logger on init

--- invisible/__init__.py
+++ invisible/__init__.py
@@ -3,6 +3,6 @@ from invisible.routes import bootstrap_routers
 
 
 def initialize_application():
-    app = TypedApp(docs_url=None, redoc_url="/docs")
+    app = TypedApp()
     bootstrap_routers(app)
     return app
--- invisible/app.py
+++ invisible/app.py
@@ -5,3 +5,7 @@ from fastapi import FastAPI
 
 class TypedApp(FastAPI):
     logger = logging.Logger
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(docs_url=None, redoc_url="/docs", **kwargs)
+        self.logger = logging.getLogger(self.title)
