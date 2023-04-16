b66161bf4f9a3bb5e9c1673cf906d38cc5426296
oskar <oskarkorgul@gmail.com>
2023-04-16 20:20:30 +0200

    fix: Add mising json encoders for metrics model

--- invisible/models/metrics.py
+++ invisible/models/metrics.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field, NonNegativeInt, PositiveInt, constr, AnyHttpUrl
 
-from invisible.models import PyObjectId
+from invisible.models import PyObjectId, ModelConfig
 
 
 class Path(BaseModel):
@@ -9,6 +9,9 @@ class Path(BaseModel):
     redirects: NonNegativeInt = 0
     tiny_urls: PositiveInt = 1
 
+    class Config(ModelConfig):
+        pass
+
 
 class Host(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
@@ -16,3 +19,6 @@ class Host(BaseModel):
     paths_ids: list[PyObjectId]
     total_redirects: NonNegativeInt = 0
     total_tiny_urls: PositiveInt = 1
+
+    class Config(ModelConfig):
+        pass
